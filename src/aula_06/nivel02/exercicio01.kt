e63de78af4635c4f4f9a3c3598754758189c5b08
Sara Alves <saraalves14@hotmail.com>
2020-09-13 18:44:32 -0300

    Continuação dos exercicios da aula 06

--- src/aula_06/nivel02/exercicio01.kt
+++ src/aula_06/nivel02/exercicio01.kt
@@ -1,79 +1,100 @@
 package aula_06.nivel02
 
-//1. Uma empresa de pesquisas precisa tabular os resultados da seguinte enquete feita a um grande quantidade de organizações:
-//"Qual o melhor Sistema Operacional para uso em servidores?"
-//As possíveis respostas são:
-//1- Windows Server 0
-//2- Unix 1
-//3- Linux 2
-//4- Netware 3
-//5- Mac OS 4
-//6- Outro 5
+/*
+Uma empresa de pesquisas precisa tabular os resultados da seguinte enquete feita a um grande quantidade de organizações:
+"Qual o melhor Sistema Operacional para uso em servidores?"As possíveis respostas são:
+1- Windows Server
+2- Unix
+3- Linux
+4- Netware
+5- Mac OS
+6- Outro
 
-//Você foi contratado para desenvolver um programa que leia o resultado da enquete
-//e informe ao final o resultado da mesma. O programa deverá ler os valores até ser
-//informado o valor 0, que encerra a entrada dos dados. Não deverão ser aceitos
-//valores além dos válidos para o programa (0 a 6). Os valores referentes a cada uma
-//das opções devem ser armazenados num vetor. Após os dados terem sido
-//completamente informados, o programa deverá calcular a percentual de cada um
-//dos concorrentes e informar o vencedor da enquete. O formato da saída foi dado
-//pela empresa, e é o seguinte:
-//Sistema Operacional Votos %
-//------------------- ----- ---
-//Windows Server 1500 17%
-//Unix 3500 40%
-//Linux 3000 34%
-//Netware 500 5%
-//Mac OS 150 2%
-//Outro 150 2%
-//------------------- -----
-//Total 8800
-//O Sistema Operacional mais votado foi o Unix, com 3500 votos,
-//correspondendo a 40% dos votos.
+Você foi contratado para desenvolver um programa que leia o resultado da enquete e informe ao final o resultado da mesma.
+O programa deverá ler os valores até ser informado o valor 0, que encerra a entrada dos dados. Não deverão ser aceitos
+valores além dos válidos para o programa (0 a 6). Os valores referentes a cada uma das opções devem ser armazenados
+num vetor. Após os dados terem sido completamente informados, o programa deverá calcular a percentual de cada um
+dos concorrentes e informar o vencedor da enquete. O formato da saída foi dadopela empresa, e é o seguinte:
+Sistema Operacional     Votos   %
+-------------------     -----   ---
+Windows Server           1500   17%
+Unix                     3500   40%
+Linux                    3000   34%
+Netware                   500    5%
+Mac OS                    150    2%
+Outro                     150    2%
+-------------------     -----
+Total                    8800O
+Sistema Operacional mais votado foi o Unix, com 3500 votos,correspondendo a 40% dos votos.
+ */
 
-const val PERGUNTA = "Qual o melhor Sistema Operacional para uso em servidores?\n " +
-        "1- Windows Server\n" +
+const val PERGUNTA = "Qual o melhor Sistema Operacional para uso em servidores?\n" + "1- Windows Server\n" +
         "2- Unix\n" +
         "3- Linux\n" +
         "4- Netware\n" +
         "5- Mac OS\n" +
         "6- Outro"
 
+const val CABECALHO = "Sistema Operacional Votos %"
+
+val arraySistemaOperacional = arrayOf("Windows Server", "Unix", "Linux", "Netware", "Mac OS", "Outro")
+val arrayVotos = arrayOf(0, 0, 0, 0, 0, 0)
+var percentualSO = ArrayList<Double>()
+var votos = 0
+var percentual: Int = 0
+
+
+
 fun main() {
-    val sistemaOperacional = arrayOf("Windows Server", "Unix", "Linux", "Netware", "Mac OS", "Outro")
+    println(PERGUNTA)
+    resultadoVotos()
+    calcularPercentual()
+    mostrarResultado()
+}
+
+fun resultadoVotos() {
     do {
+        val respostaSO = readLine()!!.toInt()
 
-        print(PERGUNTA)
-        val numeroSO = readLine()!!.toInt()
-
-        if(numeroSO < 0 || numeroSO > 6){
-            println("Opção Invalida")
+        if(respostaSO == 0) break
+        if(respostaSO in 1..6) {
+            arrayVotos[respostaSO - 1]++
+            votos++
         } else {
-
-            sistemaOperacional[numeroSO - 1]
-            var windows = 0
-            var unix = 0
-            var linux = 0
-            var netware = 0
-            var mac = 0
-            var outro = 0
-            when(numeroSO){
-                1 -> windows
-                2 -> unix
-                3 -> linux
-                4 -> netware
-                5 -> mac
-                6 -> outro
-            }
+            println("Voto invalido")
+            continue
         }
-//sistemaOperacional[0]
-//sistemaOperacional[1]
-//sistemaOperacional[2]
-//sistemaOperacional[3]
-//sistemaOperacional[4]
-//sistemaOperacional[5]
+    } while (true)
+}
 
-    } while (numeroSO != 0)
+fun calcularPercentual() {
+
+    for (i in 0..5) {
+        percentual = arrayVotos[i] * 100 / votos
+        percentualSO.add(percentual.toDouble())
+    }
+}
+
+fun vencedor(): String {
+    var maisVotos = 0
+    var aux = 0
+
+    for (x in 0..5)
+        if (arrayVotos[x] > maisVotos) {
+            maisVotos = arrayVotos[x]
+            aux = x
+        }
+    return "Sistema Operacional mais votado foi o ${arraySistemaOperacional[aux]}, com ${arrayVotos[aux]} votos, correspondendo a ${percentualSO[aux]}% dos votos"
+}
+
+fun mostrarResultado() {
+    println(CABECALHO)
+    println("--------------------   ------ ---")
+    for (i in 0..5) {
+        println("${arraySistemaOperacional[i]}                   ${arrayVotos[i]} ${percentualSO[i]}")
+    }
+    println("-----------------------------------------------------")
+    println(vencedor())
 
 }
 
--- src/aula_06/nivel02/exercicio02.kt
+++ src/aula_06/nivel02/exercicio02.kt
@@ -6,6 +6,26 @@ package aula_06.nivel02
 //mínimo é de 10 reais e o máximo de 600 reais. O programa não deve se preocupar
 //com a quantidade de notas existentes na máquina.
 
+const val PERGUNTA_QUANTO_SACAR = "Informe o valor que deseja sacar: "
 fun main() {
 
+    var notas = arrayOf(100, 50, 10, 5, 1)
+    var quantidadeNotas = arrayOf(0, 0, 0, 0, 0)
+
+    print(PERGUNTA_QUANTO_SACAR)
+    var valorSaque = readLine()!!.toInt()
+
+    if (valorSaque in 10..600) {
+        for (count in 0..4) {
+            if (valorSaque != 0) {
+                quantidadeNotas[count] = valorSaque / notas[count]
+                valorSaque %= notas[count]
+
+                println("Notas de ${notas[count]}: ${quantidadeNotas[count]}")
+            } else break
+        }
+
+    } else {
+        println("Informe um valor entre R$10 a R$600")
+    }
 }
